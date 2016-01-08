ec9e9bf94f1516d4a0cb71bf45a5fe02eac9a049
Donovan Lange <dolange@microsoft.com>
2016-01-08 11:52:30 +0100

    Add some json

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,6 +1,59 @@
 module Barista where
 
+
 import Graphics.Element as Element exposing (Element)
 
 main : Element
-main = Element.show "Barista"
+main = Element.show json
+
+json = """
+{
+  "services": {
+    "statsite": {
+      "tags": [],
+      "nodes": [
+        {
+          "name": "abc",
+          "tags": []
+        },
+        {
+          "name": "xyz",
+          "tags": []
+        }
+      ]
+    }
+  },
+  "nodes": [
+    {
+      "name": "abc",
+      "ip": "10.1.0.12",
+      "services": ["consul", "statsite", "redis"],
+      "state": {
+        "metadata": {
+          "created_at": "1111111.11",
+          "size": "Basic_A1",
+          "location": "westeurope",
+          "resource_group": "nathan-test-1"
+        },
+        "services": {
+          "statsite": {
+            "image": "wakeful/wake-statsite:latest",
+            "ports": [
+              {
+                "incoming": 8125,
+                "outgoing": 8125,
+                "udp": true
+              }
+            ],
+            "env": {
+              "TEST": "1"
+            },
+            "restart": "always",
+            "tags": ["statsd", "udp"],
+            "checks": []
+          }
+        }
+      }
+    ]
+  }
+}"""
