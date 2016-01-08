8ab52005679df562ef6d743764d7bfbf7d7753f6
Ryan Levick <ryan.levick@microsoft.com>
2016-01-08 17:22:07 +0100

    Get parsing workin', yo

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -5,77 +5,103 @@ import Graphics.Element as Element exposing (Element)
 import Dict exposing (Dict)
 
 main : Element
-main = Element.show <| decodeJson json
+main = Element.show <| decodeJson fakeData
+
+type alias Cluster =
+  { name: String
+  , location: String
+  , resourceGroup: String
+  , storageAccount: String
+  , sshProxy: String
+  , services: Dict String ServiceSummary
+  , nodes: Dict String NodeInfo
+  }
 
 type alias ServiceSummary =
-     { name: String
-     , tags : List String
-     , nodes : List NodeSummary
-     }
-
-type alias Node =
-     { name : String
-     , ip : String
-      , services : List Service
-}
-
-type alias NodeSummary =
-  { name : String
+  { instances: InstancesAmount
+  , env : Dict String String
+  , version : String
   }
 
-type alias NodeMetadata =
-    { createdAt : String
-    , size : String
-    , location : String
-    , resourceGroup : String
-    }
+type alias NodeInfo =
+  { createdAt : String
+  , size : String
+  , ip : String
+  , registered : Bool
+  , healthy : Bool
+  , services : Dict String ServiceInfo
+  }
 
-type alias Service =
+type alias ServiceInfo =
   { image : String
-  , ports : List PortMapping
-  , env : Dict String String
-  , restart : RestartFrequency
+  , registered : Bool
+  , healthy : Bool
   , tags : List String
-}
+  , registeredPort : Int
+  , expostedPorts : List PortMapping
+  }
 
 type alias PortMapping =
-    { incoming : Int
-    , outgoing : Int
-    , udp : Bool
-    }
-
-type RestartFrequency = Always | Never
-
-type alias ClusterInfo =
-  { services: List ServiceSummary
-  , nodes: List Node
+  { incoming : Int
+  , outgoing : Int
+  , udp : Bool
   }
 
-decodeJson : String -> Result String ClusterInfo
+type InstancesAmount  = EveryNode | Range Int Int
+
+type alias JsonParseError = String
+decodeJson : String -> Result JsonParseError Cluster
 decodeJson jsonString =
-  Json.decodeString clusterInfo jsonString
+  Json.decodeString cluster jsonString
 
-clusterInfo: Json.Decoder ClusterInfo
-clusterInfo =
-  Json.object2 ClusterInfo
-    ("services" := (Json.list serviceSummary))
-    ("nodes" := (Json.list node))
+cluster: Json.Decoder Cluster
+cluster =
+  Json.object7 Cluster
+    ("name"            := Json.string)
+    ("location"        := Json.string)
+    ("resource_group"  := Json.string)
+    ("storage_account" := Json.string)
+    ("sshproxy"        := Json.string)
+    ("services"        := (Json.dict serviceSummary))
+    ("nodes"           := (Json.dict nodeInfo))
 
-node : Json.Decoder Node
-node =
-  Json.object3 Node
-    ("name" := Json.string)
+serviceSummary : Json.Decoder ServiceSummary
+serviceSummary =
+  Json.object3 ServiceSummary
+    ("instances" := instancesAmount)
+    ("env" := (Json.dict Json.string))
+    ("version" := Json.string)
+
+instancesAmount : Json.Decoder InstancesAmount
+instancesAmount =
+  let
+    everyNode =
+      Json.object1 (always EveryNode) ("every_node" := Json.bool)
+    range =
+      Json.object2 Range ("minimum" := Json.int) ("maximum" := Json.int)
+  in
+    Json.oneOf [everyNode, range]
+
+
+nodeInfo : Json.Decoder NodeInfo
+nodeInfo =
+  Json.object6 NodeInfo
+    ("created_at" := Json.string)
+    ("size" := Json.string)
     ("ip" := Json.string)
-    ("services" := (Json.list service))
+    ("registered" := Json.bool)
+    ("healthy" := Json.bool)
+    ("services" := (Json.dict serviceInfo))
 
-service : Json.Decoder Service
-service =
-  Json.object5 Service
-    ("image"   := Json.string)
-    ("ports"   := (Json.list portMapping))
-    ("env"     := (Json.dict Json.string))
-    ("restart" := restartFrequency)
-    ("tags"    := (Json.list Json.string))
+serviceInfo : Json.Decoder ServiceInfo
+serviceInfo =
+  Json.object6 ServiceInfo
+    ("image"           := Json.string)
+    ("registered"      := Json.bool)
+    ("healthy"         := Json.bool)
+    ("tags"            := (Json.list Json.string))
+    ("registered_port" := Json.int)
+    ("exposed_ports"   := (Json.list portMapping))
 
 portMapping : Json.Decoder PortMapping
 portMapping =
@@ -84,95 +110,67 @@ portMapping =
     ("outgoing" := Json.int)
     ("udp"      := Json.bool)
 
-restartFrequency : Json.Decoder RestartFrequency
-restartFrequency =
-  let
-      determineFrequency frequencyString =
-        case frequencyString of
-          "always" -> Json.succeed Always
-          "never"  -> Json.succeed Never
-          str      -> Json.fail ("An invalid 'restart' string ("  ++  str ++ ") was provided")
-  in
-    Json.string `Json.andThen` determineFrequency
+-- view : Cluster -> Html
 
-serviceSummary : Json.Decoder ServiceSummary
-serviceSummary =
-  Json.object3 ServiceSummary
-    ("name" := Json.string)
-    ("tags" := (Json.list Json.string))
-    ("nodes" := (Json.list nodeSummary))
 
-nodeSummary : Json.Decoder NodeSummary
-nodeSummary =
-  Json.object1 NodeSummary
-    ("name" := Json.string)
-
-json = """
+fakeData : String
+fakeData = """
 {
-  "services": [
-    {
-      "name": "statsite",
-      "tags": [],
-      "nodes": [
-        {
-          "name": "abc"
-        },
-        {
-          "name": "xyz"
-        }
-      ]
+  "name": "nathan-1",
+  "location": "westeurope",
+  "resource_group": "nathan-1",
+  "storage_account": "nathan1",
+  "sshproxy": "40.115.38.250",
+  "services": {
+    "statsite": {
+      "instances": {
+        "every_node": true
+      },
+      "env": {},
+      "version": "abc"
+    },
+    "proxy": {
+      "instances": {
+        "minimum": 4,
+        "maximum": 4
+      },
+      "env": {
+        "PORT": "8000"
+      },
+      "version": "abc"
     }
-  ],
-  "nodes": [
-    {
+  },
+  "nodes": {
+    "abc": {
+      "created_at": "111111.11",
+      "size": "Basic_A1",
       "name": "abc",
-      "ip": "10.1.0.12",
-      "location": "westeurope",
-      "resource_group": "nathan-test-1",
-      "services": [
-        {
-          "name": "consul",
-          "image": "wakeful/consul:latest",
-          "state": "running",
-          "ports": [
-            {
-              "incoming": 8500,
-              "outgoing": 8500,
-              "udp": false
-            }
-          ],
-          "env": {},
-          "restart": "always",
-          "tags": ["consul"]
+      "ip": "10.1.0.10",
+      "registered": true,
+      "healthy": true,
+      "services": {
+        "proxy": {
+          "image": "plum/wake-proxy:abc",
+          "registered": true,
+          "healthy": true,
+          "tags": ["platform:java", "platform:scala", "version:abc"],
+          "registered_port": 31006,
+          "exposed_ports": []
         },
-        {
-          "name": "statsite",
-          "image": "wakeful/wake-statsite:latest",
-          "state": "running",
-          "ports": [
-            {
-              "incoming": 8125,
-              "outgoing": 8125,
-              "udp": true
-            }
-          ],
-          "env": {
-            "TEST": "1"
-          },
-          "restart": "always",
-          "tags": ["statsd", "udp"]
-        },
-        {
-          "name": "redis",
-          "image": "wakeful/redis:latest",
-          "state": "stopped",
-          "ports": [],
-          "env": {},
-          "restart": "always",
-          "tags": ["redis"]
+        "statsite": {
+          "image": "plum/wake-statsite:abc",
+          "registered": true,
+          "healthy": true,
+          "tags": ["platform:c", "version:abc"],
+          "registered_port": 8125,
+          "exposed_ports": [{
+            "incoming": 8125,
+            "outgoing": 8125,
+            "udp": true
+          }]
         }
-      ]
+      }
     }
-  ]
+  }
 }
 """
