fa453240250aa0339dcb0eed3fed36c49f2e3690
Donovan Lange <dolange@microsoft.com>
2016-01-08 12:46:00 +0100

    Basic types!

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,11 +1,51 @@
 module Barista where
 
-
+import Dict exposing (Dict)
 import Graphics.Element as Element exposing (Element)
 
 main : Element
 main = Element.show json
 
+type alias ServiceSummary = 
+     { name : String 
+     , tags : List String 
+     , nodes : List String
+     } 
+
+type alias Node = 
+     { name : String 
+     , ip : List String
+     , state: RunningState
+     , services : List Service
+     } 
+
+type RunningState = Running | Off
+
+type alias NodeMetadata = 
+    { createdAt : String
+    , size : String
+    , location : String
+    , resourceGroup : String
+    }
+
+type alias Service =
+    { image : String
+    , state : RunningState
+    , ports : List PortMapping
+    , env : Dict String String
+    , restart : RestartFrequency
+    , tags : List String
+    , checks : List String
+    }
+
+type alias PortMapping =
+    { incoming : Int
+    , outgoing : Int
+    , udp : Bool
+    }
+
+type RestartFrequency = Always | Never
+    
 json = """
 {
   "services": {
@@ -13,12 +53,10 @@ json = """
       "tags": [],
       "nodes": [
         {
-          "name": "abc",
-          "tags": []
+          "name": "abc"
         },
         {
-          "name": "xyz",
-          "tags": []
+          "name": "xyz"
         }
       ]
     }
@@ -27,33 +65,49 @@ json = """
     {
       "name": "abc",
       "ip": "10.1.0.12",
-      "services": ["consul", "statsite", "redis"],
-      "state": {
-        "metadata": {
-          "created_at": "1111111.11",
-          "size": "Basic_A1",
-          "location": "westeurope",
-          "resource_group": "nathan-test-1"
+      "state": "running",
+      "metadata": {
+        "created_at": "1111111.11",
+        "size": "Basic_A1",
+        "location": "westeurope",
+        "resource_group": "nathan-test-1"
+      },
+      "services": {
+        "consul": {
+          "image": "wakeful/consul:latest",
+          "state": "running",
+          "ports": [
+            {
+              "incoming": 8500,
+              "outgoing": 8500
+            }
+          ],
+          "env": {},
+          "restart": "always",
+          "tags": ["consul"]
         },
-        "services": {
-          "statsite": {
-            "image": "wakeful/wake-statsite:latest",
-            "ports": [
-              {
-                "incoming": 8125,
-                "outgoing": 8125,
-                "udp": true
-              }
-            ],
-            "env": {
-              "TEST": "1"
-            },
-            "restart": "always",
-            "tags": ["statsd", "udp"],
-            "checks": []
-          }
+        "statsite": {
+          "image": "wakeful/wake-statsite:latest",
+          "state": "running",
+          "ports": {
+              "incoming": 8125,
+              "outgoing": 8125,
+              "udp": true
+          },
+          "env": {
+            "TEST": "1"
+          },
+          "restart": "always",
+          "tags": ["statsd", "udp"]
+        },
+        "redis": {
+          "image": "wakeful/redis:latest",
+          "state": "stopped",
+          "restart": "always",
+          "tags": ["redis"]
         }
       }
-    ]
-  }
-}"""
+    }
+  ]
+}
+"""
